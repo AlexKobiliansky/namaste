56ce7e086399105a82c585cd62d48bceb85891aa
alexey kobilianskiy <alex_kobilianskiy@bigmir.net>
2018-12-06 22:10:50 +0200

    team section complete

--- app/js/common.js
+++ app/js/common.js
@@ -1,9 +1,14 @@
 $(document).ready(function() {
 
+    /**
+     * start HEIGHTESS functionality
+     */
     function heightses() {
         if ($(window).width()>=479) {
             $(".service-item-title").matchHeight();
             $(".service-item-desc").matchHeight();
+            $(".team-item-name").matchHeight();
+            $(".team-item-desc").matchHeight();
         }
 
     }
@@ -13,7 +18,35 @@ $(document).ready(function() {
     });
 
     heightses();
+    /**
+     * end HEIGHTESS functionality
+     */
 
+    var $teamSlider = $('.team-slider').owlCarousel({
+        loop:true,
+        margin:30,
+        navText: ["", ""],
+        nav: true,
+        responsive:{
+            0:{
+                items:1,
+                nav: false
+            },
+            481:{
+                items:2
+            },
+            969:{
+                items:3
+            },
+            1201:{
+                items:4
+            }
+        }
+    });
+
+    $teamSlider.on('changed.owl.carousel', function(event) {
+    heightses();
+    });
 
 
 
--- gulpfile.js
+++ gulpfile.js
@@ -30,6 +30,7 @@ gulp.task('js', ['common-js'], function() {
 		'app/libs/jquery/dist/jquery.min.js',
         'app/libs/equalHeights/jquery.equalheights.min.js',
         'app/libs/matchHeight/jquery.matchHeight-min.js',
+        'app/libs/owlCarousel/owl.carousel.min.js',
 		'app/js/common.min.js', // Всегда в конце
 		])
 	.pipe(concat('scripts.min.js')) // минимизируем и соединяем в один файл
